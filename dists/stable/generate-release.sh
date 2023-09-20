48144603ad57c4ef4d73690717b09ab1a2a92c7c
halak0013 <zumruduanka0013@gmail.com>
2023-09-20 16:25:18 +0300

    testing key problem

--- dists/stable/generate-release.sh
+++ dists/stable/generate-release.sh
@@ -32,3 +32,7 @@ do_hash "SHA256" "sha256sum"
 cat ~/projelerimiz2/yazilim/bash/apt_repo_test/apt-repo/dists/stable/Release | gpg --default-key "Muhammet Halak" -abs --clearsign > ~/projelerimiz2/yazilim/bash/apt_repo_test/dists/stable/InRelease
 
 cat ~/projelerimiz2/yazilim/bash/apt_repo_test/dists/stable/Release | gpg --default-key "Muhammet Halak" -abs > ~/projelerimiz2/yazilim/bash/apt_repo_test/dists/stable/Release.gpg
+
+echo "deb [arch=amd64 signed-by=$HOME/example/pgp-key.public] http://127.0.0.1:8000/apt-repo stable main" | sudo tee /etc/apt/sources.list.d/example.list
+
+sudo echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/Release.gpg] https://halak0013.github.io/apt_repo_test/ stable main" | sudo tee /etc/apt/sources.list.d/testrepo.list
